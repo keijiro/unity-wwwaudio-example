1dc0d509fe9eb091aa31d8261951917659953fb9
keijiro <keijiro@gmail.com>
2012-07-06 10:14:35 +0900

    Changed to use PlayOneShot, and fixed some bugs.

--- Assets/BGMLoader.js
+++ Assets/BGMLoader.js
@@ -1,44 +1,51 @@
 #pragma strict
 
-var targetFilename = "test.mp3";
-var urlBase = "https://github.com/keijiro/unity-wwwaudio-example/raw/master/Data/";
+var targetFilename : String;
+var urlBase : String;
 
+private var clip : AudioClip;
 private var message = "";
 
 private function GetCachedFilePath() {
-    return System.IO.Path.Combine(Application.temporaryCachePath, "test.mp3");
+    return System.IO.Path.Combine(Application.temporaryCachePath, targetFilename);
 }
 
 private function TryLoadAndPlay() {
     var cachePath = GetCachedFilePath();
     
-    message += "Checking cached file. (" + cachePath + ")\n";
+    message += "Checking cached file (" + cachePath + ")\n";
     var www = WWW("file:" + cachePath);
     
     yield www;
 
     if (www.error) {
-        message += "Not found. ("  + www.error + ")\n";
+        message += "Not found ("  + www.error + ")\n";
     } else {
-        audio.clip = www.GetAudioClip(false, false, AudioType.MPEG);
-        audio.Play();
+        clip = www.GetAudioClip(false, true, AudioType.MPEG);
         message += "Now playing.\n";
+        while (true) {
+            audio.PlayOneShot(clip);
+            yield WaitForSeconds(clip.length);
+        }
     }
 }
 
 
 function Start() {
     yield TryLoadAndPlay();
-    if (audio.clip) return;
     
-    message += "Downloading...\n";
-    var www = WWW("https://github.com/keijiro/unity-wwwaudio-example/raw/master/Data/test.mp3");
+    message += "Downloading (" + urlBase + targetFilename + ")\n";
+    var www = WWW(urlBase + targetFilename);
     yield www;
     
-    message += "Writing to the cache.\n";
-    System.IO.File.WriteAllBytes(GetCachedFilePath(), www.bytes);
-    
-    yield TryLoadAndPlay();
+    if (www.error) {
+        message += "Can't download ("  + www.error + ")\n";
+    } else {
+        message += "Writing to the cache.\n";
+        System.IO.File.WriteAllBytes(GetCachedFilePath(), www.bytes);
+        
+        yield TryLoadAndPlay();
+    }
 }
 
 function OnGUI() {
